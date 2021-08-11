e5f510aa56f64d0b9dfe3d4fd337765db7064dd0
Davy de Lima Carvalho <davy.carvalho@ambevtech.com.br>
2021-08-11 00:05:52 -0300

    finished

--- main.py
+++ main.py
@@ -1,24 +1,85 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
+
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Video(name = {name}, views = {views}, likes = {likes})"
+
+# db.create_all() 
+# This method creates the database, so it should run only one time, 
+# or else it will override the data in the db
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the video is required!", required=True)
 video_put_args.add_argument("views", type=int, help="Views of the video is required!", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes of the video is required!", required=True)
 
-videos = {}
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument("name", type=str, help="Name of the video is required!")
+video_update_args.add_argument("views", type=int, help="Views of the video is required!")
+video_update_args.add_argument("likes", type=int, help="Likes of the video is required!")
+
+resource_fields = {
+    "id" : fields.Integer,
+    "name" : fields.String,
+    "views" : fields.Integer,
+    "likes" : fields.Integer
+}
 
 class Video(Resource):
+    @marshal_with(resource_fields)
     def get(self, video_id):
-        return videos[video_id]
+        result = VideoModel.query.filter_by(id = video_id).first()
+        if not result:
+            abort(404, message="Video not found")
+        return result
 
+    @marshal_with(resource_fields)
     def put(self, video_id):
         args = video_put_args.parse_args()
-        videos[video_id] = args
-        return videos[video_id], 201
+        result = VideoModel.query.filter_by(id = video_id).first()
+        if result:
+            abort(409, message = "Video Id taken...")
+        video = VideoModel(id=video_id, name=args["name"], views=args["views"], likes=args["likes"])
+        db.session.add(video)
+        db.session.commit()
+        return video, 201
+
+    def patch(self, video_id):
+        args = video_put_args.parse_args()
+        result = VideoModel.query.filter_by(id = video_id).first()
+        if not result:
+            abort(404, message = "Video does not exist, cannot update!")
+
+        if args["name"]:
+            result.name = args["name"]
+        if args["views"]:
+            result.views = args["views"]
+        if args["likes"]:
+            result.likes = args["likes"]
+
+        db.session.commit()
+
+        return result
+
+    def delete(self, video_id):
+        result = VideoModel.query.filter_by(id = video_id).first()
+        if not result:
+            abort(409, message = "Video does not exist, cannot delete!")
+        db.session.delete(result)
+        db.session.commit()
+        return "", 204
 
 api.add_resource(Video, "/video/<int:video_id>")
 
--- test.py
+++ test.py
@@ -2,8 +2,15 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "video/1", {"name":"nutela", "likes":10, "views": 11000})
-print(response.json())
+data = [{"name": "How to get smart", "likes": 456, "views": 554436},
+        {"name": "How to create a database", "likes": 66, "views": 110876800},
+        {"name": "How to read pdf usin Delphi", "likes": 140, "views": 1145000},
+        {"name": "How to create a variable", "likes": 710, "views": 110023450}]
+
+for i in range(len(data)):
+    response = requests.get(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
 input()
-response = requests.post(BASE + f"video/1")
-print(response.json())
+response = requests.delete(BASE + "video/0")
+print(response)
