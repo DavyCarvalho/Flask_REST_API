1fa963d2ea3737416706fa5402f1d4fd3239f849
Davy de Lima Carvalho <davy.carvalho@ambevtech.com.br>
2021-08-10 22:15:07 -0300

    remove

--- test.py
+++ test.py
@@ -2,10 +2,8 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-# name = input("name?\n")
-
-response = requests.get(BASE + f"video/1", {"name":"nutela", "likes":10, "views": 11000})
+response = requests.get(BASE + "video/1", {"name":"nutela", "likes":10, "views": 11000})
 print(response.json())
 input()
-response = requests.get(BASE + f"video/1")
+response = requests.post(BASE + f"video/1")
 print(response.json())
